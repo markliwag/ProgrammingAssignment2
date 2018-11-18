b36f09009e7f46db986f60bb1ca6e1eede4ebc5a
unknown <markchristiand.liwag@yahoo.com>
2018-11-18 07:46:36 -0800

    ProgrammingAssignment2 about Matrices

--- cachematrix.R
+++ cachematrix.R
@@ -10,18 +10,18 @@
 
 
 makeCacheMatrix <- function(x = matrix()) {
-	invMatrix <- NULL
+	invMat <- NULL
 	
 	#set the value of the Matrix 
-	setMatrix <- function (y) {
+	setMat <- function (y) {
 	x <<- y
-	invMatrix <<- NULL
+	invMat <<- NULL
 }
 
-	getMatrix <- function() x 						#gets the value of the matrix
-	setInverse <- function(inverse) invMatrix <<- inverse		#sets the value of the invertible matrix
-	getInverse <- function() invMatrix					#get the value of the invertible matrix
-	list(setMatrix = setMatrix, getMatrix = getMatrix,setInverse = setInverse, getInverse = getInverse)	 
+	getMat <- function() x 						#gets the value of the matrix
+	setInv <- function(inverse) invMat <<- inverse		#sets the value of the invertible matrix
+	getInv <- function() invMat					#get the value of the invertible matrix
+	list(setMat = setMat, getMat = getMat,setInv = setInv, getInv = getInv)	 
 
 }
 ## Write a short comment describing this function
@@ -30,23 +30,23 @@ makeCacheMatrix <- function(x = matrix()) {
 ##input and checks the inverse matrix from makeCacheMatrix(matrix)if it has any value in it or not.
 ##In case inverse matrix from makeCacheMatrix((matrix) is empty, it gets the original matrix data
 ##and set the invertible  matrix by using the solve function.In case inverse matrix from makeCacheMatrix((matrix) 
-##has some value in it, it returns a message  "Getting Cached Invertible Matrix" along with the cached object
+##has some value in it, it returns a message  "Giving Back Cached Inverted Matrix" along with the cached object
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 	
 	##Get the value of the invertible matrix from the makeCacheMatrix function
-	invMatrix <- x$getInverse()
-	if(!is.null(invMatrix)) {						#if inverse matrix is not NULL
-		message("Getting Cached Invertible Matrix")		#Type message: Getting Cached Invertible Matrix 
-		return(invMatrix)							#return the invertible matrix
+	invMat <- x$getInv()
+	if(!is.null(invMat)) {						#if inverse matrix is not NULL
+		message("Giving Back Cached Inverted Matrix")		#Type message: Giving Back Cached Inverted Matrix 
+		return(invMat)							#return the invertible matrix
 }
 
 	#if value of the invertible matrix is NULL then
-	MatrixData <- x$getMatrix()                     #get the original Matrix Data 
-	invMatrix <- solve(MatrixData, ...)             #use solve function to inverse the matrix
-	x$setInverse(invMatrix)                         #set the invertible matrix 
-	 return(invMatrix)                               #return the invertible matrix
+	MatrixData <- x$getMat()                     #get the original Matrix Data 
+	invMat <- solve(MatrixData, ...)             #use solve function to inverse the matrix
+	x$setInv(invMat)                         #set the invertible matrix 
+	 return(invMat)                               #return the invertible matrix
 }
 
 
